4811f011e871cde9aa2ad9b94301623c4e0e5667
Jeonghan Oh <myown1209@gmail.com>
2023-03-14 18:02:52 +0900

    mercury

--- 03/07/test02.js
+++ 03/07/test02.js
@@ -1,27 +1,66 @@
-function selectAll(){
+// 첫번째 체크박스에 동의를 해주면 밑에 4개의 체크박스도 동의를 활성화 시켜준다.
+// 첫번째 체크박스에 체크를 풀어주면 밑에 4개의 체크박스도 동의를 비활성화 시킨다.
+function selectAll() {
     var chk1 = document.getElementById('chk1');
 
     const checkboxes = document.getElementsByName('clause');
 
+    var str = chk1.src;
+
     checkboxes.forEach((checkbox) => {
-        checkbox.checked = chk1.checked;
+        if (str != "http://127.0.0.1:5500/03/07/checkOn.png") {
+            chk1.src = "checkOn.png";
+            checkbox.src = "checkOn.png";
+        }
+        else {
+            chk1.src = "checkOff.png";
+            checkbox.src = "checkOff.png";
+        }
     })
 }
 
-function fnForChk1(select){
+// 두번째 체크박스부터 모두 동의 되어 있을 경우 가장 위 체크박스에 동의를 활성화 시켜준다.
+// 두번째 체크박스부터 동의 되어 있지 않는 것이 있다면 가장 위 체크박스의 동의를 비활성화 시켜준다.
+function fnForChk1(select) {
     var chk1 = document.getElementById('chk1');
-    if(select.checked==false){
-        chk1.checked=false;
+    if (select.src == "http://127.0.0.1:5500/03/07/checkOff.png") {
+        select.src = "checkOn.png";
+    }
+    else{
+        select.src = "checkOff.png";
+        chk1.src = "checkOff.png";
     }
 
     const checkboxes = document.getElementsByName('clause');
 
-    var cnt=0;
-    checkboxes.forEach((checkbox) =>{
-        if(checkbox.checked == false)
+    var cnt = 0;
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.src == "http://127.0.0.1:5500/03/07/checkOff.png")
             cnt++;
     })
 
-    if(cnt == 0)
-        chk1.checked=true;
+    if (cnt == 0)
+        chk1.src = "checkOn.png";
+}
+
+// 필수 약관에 동의가 되어 있다면 다음 가입페이지로 넘어가는 함수
+function next(){
+    const checkboxes = document.getElementsByName('clause');
+
+    var indx=0;
+    var cnt=0;
+    checkboxes.forEach((checkbox)=>{
+        if(indx==0 || indx==1){ 
+            if (checkbox.src == "http://127.0.0.1:5500/03/07/checkOn.png"){
+                cnt++;
+            }
+        }
+
+        indx++;
+    })
+
+    if(cnt == 2){
+        location.href='test03.html';
+    }
+
 }
---
+++ 03/07/test03.js
@@ -0,0 +1,233 @@
+var chk = [];
+
+// 포커스가 해제 되었을 때 실행하는 함수
+function OnBlur(id, id2) {
+    var doc = document.getElementById(id);
+    var doc2 = document.getElementById(id2);
+
+
+    //텍스트박스에 빈칸인 경우
+    if (doc2.value == "" && id2 != 'textbox6' && id2 != 'textbox6_2' && id2 != 'selectBox') {
+        doc.innerHTML = "필수 정보입니다.";
+        doc.style = "color:red";
+    }
+    else { //텍스트박스에 값이 들어있는 경우
+        if (id2 == 'textbox1') { // 아이디를 입력받는 텍스트박스인 경우
+            textbox1();
+        }
+        else if (id2 == 'sb1') { // 성별을 선택받는 셀렉트박스일 때
+            sb1();
+        }
+        else if (id2 == 'textbox10') { // 전화번호를 입력받는 텍스트박스인 경우
+            textbox10();
+        }
+        else if (id2 == 'textbox11') { // 인증번호를 입력받는 텍스트박스인 경우
+            textbox11();
+        }
+        else if (id2 == 'textbox5') {  // 이름을 입력받는 텍스트박스인 경우
+            textbox5();
+        }
+        else if (id2 == 'textbox6') { // 생년을 입력받는 텍스트박스인 경우
+            birthday();
+        }
+        else if (id2 == 'textbox6_2') { // 일을 입력받는 텍스트박스인 경우
+            birthday();
+        }
+        else if (id2 == 'selectBox') { // 월을 입력받는 텍스트박스인 경우
+            birthday();
+        }
+    }
+}
+
+
+function birthday() {
+    var doc = document.getElementById('pilsu5');
+
+    var reg = /^[0-9]+$/;
+    var years = document.getElementById('textbox6');
+    var date = document.getElementById('textbox6_2');
+    var months = document.getElementById('selectBox');
+    var x = -1, y = -1, z = -1;
+
+    var dt = new Date();
+    var curDt = new Date();
+
+    // 입력받은 일과 정규식이 매칭하는지 체크한다.
+    if (date.value.match(reg)) {
+        var z = parseInt(date.value);
+        dt.setDate(z);
+    }
+    else {
+        doc.innerHTML = "태어난 일(날짜) 2자리를 정확하게 입력하세요.";
+        doc.style = "color:red";
+    }
+
+    // 입력받은 월과 정규식이 매칭하는지 체크한다.
+    if (months.value.match(reg)) {
+        var y = parseInt(months.value) - 1;
+        dt.setMonth(y);
+    }
+    else {
+        doc.innerHTML = "태어난 월을 선택하세요.";
+        doc.style = "color:red";
+    }
+
+    // 입력받은 년과 정규식이 매칭하는지 체크한다.
+    if (years.value.match(reg)) {
+        x = parseInt(years.value);
+        dt.setFullYear(x);
+    }
+    else {
+        doc.innerHTML = "태어난 년도 4자리를 정확하게 입력하세요.";
+        doc.style = "color:red";
+    }
+
+    // 생년월일 모든 칸에 제대로된 입력을 받은 경우
+    if (x != -1 && y != -1 && z != -1) {
+        doc.innerHTML = "";
+        // true 라는 뜻으로 1을 셋팅한다.
+        chk[4] = 1;
+    }
+}
+
+// 이름
+function textbox5() {
+    var doc = document.getElementById('pilsu4');
+    var doc2 = document.getElementById('textbox5');
+
+    var regExp = /^[가-힣a-zA-Z]+$/;
+
+    if (doc2.value.match(regExp)) {
+        chk[3] = 1;
+        doc.innerHTML = "";
+    }
+}
+function textbox11() {
+    var doc = document.getElementById('pilsu6');
+    var doc2 = document.getElementById('textbox11');
+
+    if (doc2.value == 123456) {
+        doc.innerHTML = "";
+        chk[7] = 1;
+    }
+}
+
+function textbox10() {
+    var doc = document.getElementById('pilsu6');
+    var doc2 = document.getElementById('textbox10');
+
+    var regExp = /^\d{3}-?\d{3,4}-?\d{4}$/;
+
+    if (doc2.value.match(regExp)) {
+        doc.innerHTML = "";
+        chk[6] = 1;
+    }
+}
+
+function sb1() {
+    var doc = document.getElementById('pilsu7');
+    var doc2 = document.getElementById('sb1');
+
+    if (doc2.value == '성별') {
+        doc.innerHTML = "필수 정보입니다.";
+        doc.style = "color:red";
+    }
+    else {
+        doc.innerHTML = "";
+        chk[5] = 1;
+    }
+}
+
+function textbox1() {
+    var doc = document.getElementById('pilsu');
+    var doc2 = document.getElementById('textbox1');
+
+    var regExp = /^[a-z0-9_-]{5,20}$/;
+
+    if (doc2.value.match(regExp)) {
+        chk[0] = 1;
+        doc.innerHTML = "멋진 아이디네요!";
+        doc.style = "color:green";
+    }
+    else {
+        doc.innerHTML = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
+        doc.style = "color:red";
+    }
+}
+
+// 올바른 패스워드인지 확인한다.
+function chkPassword() {
+    var doc = document.getElementById('textbox3');
+    var reg = /^.*(?=^.{8,16}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
+
+    if (doc.value.match(reg) == null) {
+        var doc2 = document.getElementById('pilsu2');
+        doc2.innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+        doc2.style = "color:red";
+
+        var doc3 = document.getElementById('lock1Img');
+        doc3.src = "/03/07/lock3_1.png"
+    }
+    else {
+        var doc3 = document.getElementById('lock1Img');
+        doc3.src = "/03/07/lock2_2.png"
+
+        var doc2 = document.getElementById('pilsu2');
+        doc2.innerHTML = "";
+
+        chk[1] = 1;
+    }
+}
+
+// 비밀번호 확인을 수행한다.
+function equalsPassword() {
+    var doc = document.getElementById('textbox3');
+    var doc2 = document.getElementById('textbox4');
+
+    if (doc.value == doc2.value) {
+        var doc3 = document.getElementById('pilsu3');
+        doc3.innerHTML = ""
+        var doc4 = document.getElementById('lock2Img');
+        doc4.src = "/03/07/lock2_2.png"
+
+        chk[2] = 1;
+    }
+    else {
+        var doc3 = document.getElementById('pilsu3');
+        doc3.innerHTML = "비밀번호가 일치하지 않습니다."
+        doc3.style = "color:red";
+    }
+}
+
+// 인증번호 받기 버튼을 눌렀을 때
+function certiNumber() {
+    alert("123456");
+
+    var doc = document.getElementById('textbox11');
+
+    doc.disabled = false;
+}
+
+// 가입하기 버튼을 눌렀을 때
+function gaibhagi() {
+    equalsPassword();
+    chkPassword();
+    textbox1();
+    sb1();
+    textbox5();
+    birthday();
+    textbox11();
+    textbox10();
+
+    for (var i = 0; i < 8; i++) {
+        if (chk[i] != 1) {
+            alert("입력을 하지 않은 항목이 있습니다.");
+            return;
+        }
+    }
+    location.href = 'test04.html';
+
+    for (var i = 0; i < 8; i++) {
+        chk[i] = 0;
+    }
+}
