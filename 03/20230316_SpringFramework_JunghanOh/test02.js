6759184910fec9442732b87bc7a2797ef812ae3f
Jeonghan Oh <myown1209@gmail.com>
2023-03-16 13:57:37 +0900

    commit

--- 03/20230316_SpringFramework_JunghanOh/test02.js
+++ 03/20230316_SpringFramework_JunghanOh/test02.js
@@ -41,21 +41,22 @@ function OnBlur(id, id2) {
 
 // 생년월일 입력란+셀렉트박스
 function input_birthday() {
+    // 경고창을 띄우기 위한 doc
     var doc = document.getElementById('pilsu5');
 
     var reg = /^[0-9]+$/;
-    var years = document.getElementById('textbox6');
-    var months = document.getElementById('selectBox');
-    var date = document.getElementById('textbox6_2');
+    // 텍스트박스 년도에 들어간 값
+    var years = document.getElementById('textbox6').value;
+    // 셀렉트박스 월에 들어간 값
+    var months = document.getElementById('selectBox').value;
+    // 텍스트박스 일에 들어간 값
+    var date = document.getElementById('textbox6_2').value;
+    // 년도,월,일이 제대로된 값이 아니면 -1 제대로된 값이면 다른 값이 된다.
     var x = -1, y = -1, z = -1;
 
-    var dt = new Date();
-    var curDt = new Date();
-
     // 입력받은 일과 정규식이 매칭하는지 체크한다.
-    if (date.value.match(reg)) {
-        z = parseInt(date.value);
-        dt.setDate(z);
+    if (date.match(reg)) {
+        z = parseInt(date);
     }
     else {
         doc.innerHTML = "태어난 일(날짜) 2자리를 정확하게 입력하세요.";
@@ -63,9 +64,8 @@ function input_birthday() {
     }
 
     // 입력받은 월과 정규식이 매칭하는지 체크한다.
-    if (months.value.match(reg)) {
-        y = parseInt(months.value) - 1;
-        dt.setMonth(y);
+    if (months.match(reg)) {
+        y = parseInt(months) - 1;
     }
     else {
         doc.innerHTML = "태어난 월을 선택하세요.";
@@ -73,9 +73,8 @@ function input_birthday() {
     }
 
     // 입력받은 년과 정규식이 매칭하는지 체크한다.
-    if (years.value.match(reg)) {
-        x = parseInt(years.value);
-        dt.setFullYear(x);
+    if (years.match(reg)) {
+        x = parseInt(years);
     }
     else {
         doc.innerHTML = "태어난 년도 4자리를 정확하게 입력하세요.";
@@ -92,11 +91,14 @@ function input_birthday() {
 
 // 이름 입력란
 function input_name() {
+    // 경고창을 띄우기 위한 doc
     var doc = document.getElementById('pilsu4');
+    // 텍스트박스의 doc2
     var doc2 = document.getElementById('textbox5');
 
     var regExp = /^[가-힣a-zA-Z]+$/;
 
+    // 이름이 올바르다면
     if (doc2.value.match(regExp)) {
         chk[3] = 1;
         doc.innerHTML = "";
@@ -105,9 +107,12 @@ function input_name() {
 
 // 인증번호 입력란
 function input_certiNum() {
+    // 경고창을 띄우기 위한 doc
     var doc = document.getElementById('pilsu6');
+    // 텍스트박스의 doc2
     var doc2 = document.getElementById('textbox11');
 
+    // 인증번호를 제대로 입력받은 경우
     if (doc2.value == 123456) {
         doc.innerHTML = "";
         chk[7] = 1;
@@ -116,11 +121,14 @@ function input_certiNum() {
 
 // 휴대폰 입력란
 function input_phone() {
+    // 경고창을 띄우기 위한 doc
     var doc = document.getElementById('pilsu6');
+    // 텍스트박스의 doc2
     var doc2 = document.getElementById('textbox10');
 
     var regExp = /^\d{3}-?\d{3,4}-?\d{4}$/;
 
+    // 휴대폰 번호가 올바르다면
     if (doc2.value.match(regExp)) {
         doc.innerHTML = "";
         chk[6] = 1;
@@ -129,13 +137,17 @@ function input_phone() {
 
 // 성별 셀렉트박스
 function input_gender() {
+    // 경고창을 띄우기 위한 doc
     var doc = document.getElementById('pilsu7');
+    // 셀렉트박스의 doc2
     var doc2 = document.getElementById('sb1');
 
+    // 셀렉트박스에서 현재 선택된 부분인 '성별'이라면
     if (doc2.value == '성별') {
         doc.innerHTML = "필수 정보입니다.";
         doc.style = "color:red";
     }
+    // 성별 이외의 선택지를 선택했다면
     else {
         doc.innerHTML = "";
         chk[5] = 1;
@@ -144,16 +156,20 @@ function input_gender() {
 
 // 아이디 입력란
 function input_id() {
+    // 경고창을 띄우기 위한 doc
     var doc = document.getElementById('pilsu');
+    // 텍스트박스의 doc2
     var doc2 = document.getElementById('textbox1');
 
     var regExp = /^[a-z0-9_-]{5,20}$/;
 
+    // 아이디가 올바르다면
     if (doc2.value.match(regExp)) {
         chk[0] = 1;
         doc.innerHTML = "멋진 아이디네요!";
         doc.style = "color:green";
     }
+    // 아이디가 올바르지 않다면
     else {
         doc.innerHTML = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
         doc.style = "color:red";
@@ -162,21 +178,28 @@ function input_id() {
 
 // 올바른 패스워드인지 확인한다.
 function chkPassword() {
+    // 텍스트박스의 doc
     var doc = document.getElementById('textbox3');
     var reg = /^.*(?=^.{8,16}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
 
+    // 비밀번호가 올바르지 않다면
     if (doc.value.match(reg) == null) {
+        // 경고창을 띄우기 위한 doc2
         var doc2 = document.getElementById('pilsu2');
         doc2.innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
         doc2.style = "color:red";
 
+        // 자물쇠 표시를 바꿔준다.
         var doc3 = document.getElementById('lock1Img');
         doc3.src = "/03/07/lock3_1.png"
     }
+    // 비밀번호가 올바르다면
     else {
+        // 자물쇠 표시를 바꿔준다.
         var doc3 = document.getElementById('lock1Img');
         doc3.src = "/03/07/lock2_1.png"
 
+        // 경고창에 공란
         var doc2 = document.getElementById('pilsu2');
         doc2.innerHTML = "";
 
@@ -186,18 +209,25 @@ function chkPassword() {
 
 // 비밀번호 확인을 수행한다.
 function equalsPassword() {
+    // 텍스트박스의 doc
     var doc = document.getElementById('textbox3');
+    // 텍스트박스의 doc2
     var doc2 = document.getElementById('textbox4');
 
+    //비밀번호가 일치한다면
     if (doc.value == doc2.value) {
+        // 경고창이 공란
         var doc3 = document.getElementById('pilsu3');
         doc3.innerHTML = ""
+        // 자물쇠 표시를 바꿔준다.
         var doc4 = document.getElementById('lock2Img');
         doc4.src = "/03/07/lock2_2.png"
 
         chk[2] = 1;
     }
+    // 비밀번호가 일치하지 않는다면
     else {
+        // 경고창을 띄우기 위한 doc
         var doc3 = document.getElementById('pilsu3');
         doc3.innerHTML = "비밀번호가 일치하지 않습니다."
         doc3.style = "color:red";
@@ -218,6 +248,7 @@ function certiNumber() {
 
     var doc = document.getElementById('textbox11');
 
+    // 인증번호를 알게된 경우 텍스트박스를 활성화 시켜준다.
     doc.disabled = false;
 }
 
